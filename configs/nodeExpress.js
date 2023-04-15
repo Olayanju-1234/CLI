c5468d0eeb4df4a6963a58018e33a3016d3c15e0
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-04-15 21:01:57 +0100

    New commit

---
+++ configs/nodeExpress.js
@@ -0,0 +1,40 @@
+const inquirer = require('inquirer');
+
+const baseConfig = {
+    builds
+    : [{ 
+        src: 'src/app.js',
+        use: '@now/node-server'
+    }],
+    routes: [
+        { src: '/.*', dest: 'src/app.js' }
+]
+}
+
+async function nodeExpressConfig(config) {
+    let mainFile = 'src/app.js';
+    
+    try {
+        const packageJSON = require('../package.json')
+        mainFile = packageJSON.main;
+    } catch (error) {
+    }
+
+    const answers = await inquirer.prompt([
+        {
+            type: 'text',
+            name: 'main',
+            message: 'What is the path to your express entry point?',
+            default: mainFile,
+        }
+    ]);
+    console.log(answers);
+    return {
+        ...baseConfig,
+        ...config
+    }
+}
+
+module.exports = {
+    nodeExpressConfig
+}
---
+++ src/app.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+
+const { nodeExpressConfig } = require('../configs/nodeExpress');
+
+const existingConfig =  fs.existsSync('now.json')
+
+async function createConfig() {
+
+    let config = {
+        version: 2,
+    };
+
+    const answers = await inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: 'What is the name of your project?',
+            default: path.basename(process.cwd())
+        },
+        {
+            type: 'list',
+            name: 'type',
+            message: 'What type of project is this?',
+            choices: ['node-express', 'static', 'react', 'static-build', 'lambda'],
+        },
+    ])
+        config.name = answers.name;
+        switch (answers.type) {
+            case 'node-express':
+                config = await nodeExpressConfig(config);
+                break;
+        }
+        console.log(config);
+}
+
+
+if (existingConfig) {
+inquirer.prompt([
+    {
+    type: 'confirm',
+    name: 'overwrite',
+    message: 'now.json exists. Do you want to overwrite the existing config?',
+    default: false
+    }
+  ]).then(answers => {
+    if (answers.overwrite) {
+      createConfig()
+    } else {
+        console.log('Exiting 👋');
+    }
+  });
+} else {
+    createConfig();
+}
+
